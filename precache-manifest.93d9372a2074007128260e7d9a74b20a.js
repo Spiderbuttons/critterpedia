27400398ba3dcc0120d31e9d4c7b970d93687ae6
Button <azurylldmm@gmail.com>
2020-03-25 02:33:37 -0400

    Updates

---
+++ precache-manifest.93d9372a2074007128260e7d9a74b20a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4906b57710ee17fc76b236bd3ca293cf",
+    "url": "/critterpedia/index.html"
+  },
+  {
+    "revision": "4e6c3f7aaf8df2587241",
+    "url": "/critterpedia/static/css/main.57c0efb7.chunk.css"
+  },
+  {
+    "revision": "0387e1e956d489dbba60",
+    "url": "/critterpedia/static/js/2.be21d47c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/critterpedia/static/js/2.be21d47c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e6c3f7aaf8df2587241",
+    "url": "/critterpedia/static/js/main.b290c471.chunk.js"
+  },
+  {
+    "revision": "0f724e1b9d4ec2f5a7ef",
+    "url": "/critterpedia/static/js/runtime-main.039caebe.js"
+  }
+]);
